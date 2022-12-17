ee6313c31d6c0a82d897c09cba37ef7e608645ca
top <levtslev@yandex.ru>
2022-12-18 00:54:33 +0300

    добавили класс main и файл README.md

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = new int[]{1,5,3,4,6,8,3,7};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+    }
+}
